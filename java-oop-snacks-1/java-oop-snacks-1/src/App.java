b4dbf7210dc45f9d9598d7f31e9ac0891f171d0b
claudia13-prog <cappellaclaudia13@gmail.com>
2024-11-21 20:59:09 +0100

    completed snack 1: studente

--- java-oop-snacks-1/java-oop-snacks-1/src/App.java
+++ java-oop-snacks-1/java-oop-snacks-1/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Studente studente1 = new Studente("Claudia", "Cappella", 28);
+
+        System.out.println(studente1.getValori());
+
     }
 }
--- java-oop-snacks-1/java-oop-snacks-1/src/Studente.java
+++ java-oop-snacks-1/java-oop-snacks-1/src/Studente.java
@@ -1,3 +1,23 @@
 public class Studente {
 
+    // Attributi
+    String nome;
+    String cognome;
+    int età;
+
+    // Metodo Costruttore
+    public Studente(String nome, String cognome, int età){
+        this.nome = nome;
+        this.cognome = cognome;
+        this.età = età;
+
+    }
+
+    public String getValori(){
+        return this.nome + " " + this.cognome + ", " + this.età + " anni";
+    }
+
+
+
+
 }
